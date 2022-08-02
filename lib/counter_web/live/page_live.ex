ae800223de0effe6a6c4887f4ee2b98d1ade1454
Manisha Singh <manishasingh1038@gmail.com>
2022-08-02 09:58:57 +0530

    commit

--- lib/counter_web/live/page_live.ex
+++ lib/counter_web/live/page_live.ex
@@ -1,11 +1,21 @@
 defmodule CounterWeb.PageLive do
   use CounterWeb, :live_view
+
+  # mount function is called once when the liveview web first connect to the server
   def mount(_params, _session, socket) do
     {:ok,
      socket
      |> assign(number: 0)}
   end
 
+  # @spec render(any) :: Phoenix.LiveView.Rendered.t()
+  # def render(assigns) do
+  #   ~L"""
+  #   <h1>Count: <%= @count %></h1>
+  #   <button>+</button>
+  #   <button>-</button>
+  #   """
+  # end
   def handle_event("inc", _params, socket) do
     {:noreply,
      socket
